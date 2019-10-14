008d7f5ea56c263abd6fa243a2a07adfe1a54544
onyskj <41900636+onyskj@users.noreply.github.com>
2019-10-14 19:07:26 +0000

    Add files via upload

--- assignment01/mainJO.py
+++ assignment01/mainJO.py
@@ -6,9 +6,10 @@ import re
 import numpy as np
 from sklearn.model_selection import train_test_split
 from itertools import product
+from scipy import optimize
 
 
-### Q1
+### Q1 ###
 
 def preprocess_line(line):
     """ Takes line of text and returns string which removes all characters from line not
@@ -32,20 +33,7 @@ def preprocess_line(line):
     return re.sub('[^a-z0.\s#]', '', line)
 
 
-### Q2
-'''
-We think that the method used to estimate probabilities in the provided model-br.en is
-add-alpha smoothing. The reason behind that judgment is that we don't find any probabilities 
-equal zero but can notice many equal to 3.333e-02 which is the inverse of vocabulary (v=30).
-This in turn suggests that the formula for conditional probablity was supplemented in the 
-numerator with +/alpha and in the denominator with +/alpha*v. That means the trigram and 
-bigram counts were zero. As a result alphas cancel out and we remain with 1/v=3.333e-02.
-Moreover, we think the estimation method cannot be interpolation nor backoff, since it's
-unlikely that we would end up with so many values of 3.333e-02, but rather values multiplied 
-by backoff weights or three lambdas (for interpolation) which would make values more varied. 
-'''
-
-### Q3
+### Q3 ###
 
 def write_to_file(model, file_name):
     """ Writes a dictionary to a text file with each line containing a key and its value
@@ -83,8 +71,8 @@ def generate_counts(infile, n):
 
     all_combos = product('#abcdefghijklmnopqrstuvwxyz0. ', repeat=n)
     if n == 1:
-        all_combos = [a for a in all_combos]
-#        all_combos = list('#abcdefghijklmnopqrstuvwxyz0. ')
+#        all_combos = [a for a in all_combos]
+        all_combos = list('#abcdefghijklmnopqrstuvwxyz0. ')
     if n == 2:
         all_combos = [a + b for a, b in all_combos]
         regex = re.compile(r'##')
@@ -137,29 +125,61 @@ def ngram_model(infile, n, add_alpha=None):
         else:
             norm_counts[key] = 0
 
-#    write_to_file(norm_counts, str(n) + '-gram.txt')
     return norm_counts
 
-
-def perplexity(infile, n_gram_model, n): #param, model_type, n, training_data, valid_data
-     """ Perplexity is the measure of how well a probability distribution predicts a sample
+def perplexity_for_optim(param, model_type, n, training_data, valid_data):
+    """ Perplexity is the measure of how well a probability distribution predicts a sample.
+        This variation of a function is used to optimise alpha for add-alpha smoothing.
     Args:
         param (int or list): int represent the alpha for add-alpha smoothing, list
         representing the lambdas for interpolation smoothing
         model_type (str): Either 'ngram add alpha' or 'interpolation'
         n (int): desired size of n_grams
-        training_data (text file): Text file to use as training data
-        valid_data (text file): Text file to use as validation or testing data
+        training_data (list): Training data set
+        valid_data (list): Validation data set
     Returns:
         (float): perplexity calculation for given model
     """
-    
-#    model = None
-#    if model_type == 'ngram add alpha':
-#        model = ngram_model(training_data, n, param)
-#    elif model_type == 'interpolation':
-#        model = interpolation(training_data, n, param)
-    
+    model = None
+    if model_type == 'ngram add alpha':
+        model = ngram_model(training_data, n, param)
+    elif model_type == 'interpolation':
+        model = interpolation(training_data, n, param)
+
+    p_log = 0
+    n_gram_count = 0
+    for line in valid_data:
+        line = preprocess_line(line)
+        for j in range(len(line) - (n-1)):
+            n_gram = line[j:j + n]
+            n_gram_count += 1
+            p_log += np.log2(model[n_gram])
+    return 2 ** (-1 / n_gram_count * p_log)
+
+def optimAddAlpha(train_set, valid_set, n):
+    """ Find the optimal alpha parameter for add-alpha trigram smoothing model
+    Returns:
+        optim.x (float):  optimized alpha parameter
+    """
+    model = 'ngram add alpha'
+    train = train_set
+    valid = valid_set
+    optim_args = (model, n, train, valid)
+    bnds=((0,1),)
+    optim = optimize.minimize(perplexity_for_optim, 0.04,bounds=bnds, args=optim_args)
+    return float(optim.x)                  
+
+def perplexity(infile, n_gram_model, n):
+    """ Perplexity is the measure of how well a probability distribution predicts a sample.
+        This is a simple perplexity function that just calculates perplexity of a document
+        using a given model, without optimising any parameters.
+    Args:
+        infile (list): a set of sequences to calculate perplexity on
+        n_gram_model (dict): an n-gram model to use in perplexity calculation
+        n (int): desired size of n_grams
+    Returns:
+        (float): perplexity calculation for given model
+    """
     p_log = 0
     n_gram_count = 0
     for line in infile:
@@ -170,35 +190,16 @@ def perplexity(infile, n_gram_model, n): #param, model_type, n, training_data, v
             p_log += np.log2(n_gram_model[n_gram])
     return 2 ** (-1 / n_gram_count * p_log)
 
-def find_optimal_alpha(alphas, validation_set, train_set, n):
-    '''
-    Trains a model on train data for different alphas and then calculates 
-    a perplexity on a validation set under this model to then pick alpha 
-    that gives the smallest perplexity.  
-    '''
-    if any(alphas<=0) or any(alphas>1):
-        raise Exception('Alpha should be > 0 and <=1')
-    alpha_smoothed_models=[dict() for i in range(len(alphas))] 
-    perplexities=np.zeros(len(alphas))
-    for i in range(len(alphas)):
-        alpha_smoothed_models[i] = ngram_model(train_set,n,alphas[i])
-        perplexities[i] = perplexity(validation_set, alpha_smoothed_models[i],n) #under validation
-
-    return alphas[perplexities.argmin()]
 
 ## English data ##
 data = open('training.en').read().splitlines()
 data_train, data_valid = train_test_split(data, test_size=0.2, shuffle=True)
 data_valid, data_test = train_test_split(data_valid, test_size=0.5, shuffle=True)
 
-perplexity(data_test,ngram_model(data_train,3, add_alpha=1),3)
-
 # Find best alpha for English #
-alphas = np.linspace(0.0000001,1,1000)
-#best_alpha = np.rouwnd(find_optimal_alpha(alphas, data_valid, data_train, 3),4)
-best_alpha = 0.047
+best_alpha=optimAddAlpha(data_train, data_valid, 3)
 
-# Trigram model + save to file #
+# Trigram model for English + save to file #
 trigram = ngram_model(data_train, 3, add_alpha=best_alpha)
 write_to_file(trigram, 'trigram.txt')
 
@@ -206,9 +207,10 @@ write_to_file(trigram, 'trigram.txt')
 ng_ngrams = {key: value for key, value in trigram.items() if key.startswith('ng') }
 write_to_file(ng_ngrams, 'ngdict.txt')
 
-#perplexity(data_valid, trigram, 3)
-#print(perplexity(data_train, trigram, 3))
-print(perplexity(data_test, trigram, 3))
+#Perplexities on validation and test set
+print(perplexity(data_train, trigram, 3)) # Perplexity of english model on the training data
+print(perplexity(data_valid, trigram, 3)) # Perplexity of english model on the validation data
+print(perplexity(data_test, trigram, 3)) # Perplexity of english model on the test data
 
 
 ## German data ##
@@ -217,16 +219,16 @@ data_trainDe, data_validDe = train_test_split(dataDe, test_size=0.2, shuffle=Tru
 data_validDe, data_testDe = train_test_split(data_validDe, test_size=0.5, shuffle=True)
 
 # Find best alpha for German #
-alphas = np.linspace(0.0000001,1,1000)
-#best_alphaDe = np.round(find_optimal_alpha(alphas, data_validDe, data_trainDe, 3),4)
-best_alphaDe = 0.1041
+best_alphaDe = optimAddAlpha(data_trainDe, data_validDe, 3)
 
-# Trigram model
+# Trigram model for German
 trigramDe = ngram_model(data_trainDe, 3, add_alpha=best_alphaDe)
 
-#print(perplexity(data_trainDe, trigramDe, 3))
-#print(perplexity(data_validDe, trigramDe, 3))
-#print(perplexity(data_testDe, trigramDe, 3))
+# Perplexities on training, validation and test set for German
+print(perplexity(data_trainDe, trigramDe, 3))
+print(perplexity(data_validDe, trigramDe, 3))
+print(perplexity(data_testDe, trigramDe, 3))
+
 
 ## Spanish data ##
 dataEs = open('training.es').read().splitlines()
@@ -234,17 +236,15 @@ data_trainEs, data_validEs = train_test_split(dataEs, test_size=0.2, shuffle=Tru
 data_validEs, data_testEs = train_test_split(data_validEs, test_size=0.5, shuffle=True)
 
 # Find best alpha for Spanish #
-alphas = np.linspace(0.0000001,1,1000)
-#best_alphaEs = np.round(find_optimal_alpha(alphas, data_validEs, data_trainEs, 3),4)
-best_alphaEs = 0.0781
+best_alphaEs = optimAddAlpha(data_trainEs, data_validEs, 3)
 
-
-# Trigram model #
+# Trigram model for Spanish #
 trigramEs = ngram_model(data_trainEs, 3, add_alpha=best_alphaEs)
 
-#print(perplexity(data_trainEs, trigramEs, 3))
-#print(perplexity(data_validEs, trigramEs, 3))
-#print(perplexity(data_testEs, trigramEs, 3))
+# Perplexities on training, validation and test set for Spanish
+print(perplexity(data_trainEs, trigramEs, 3))
+print(perplexity(data_validEs, trigramEs, 3))
+print(perplexity(data_testEs, trigramEs, 3))
 
 
 ### Q4 ###
@@ -278,7 +278,6 @@ def generate_text(model, n, length):
         text += sample
 
     print('Generated Text: ', text)
-#    return text
 
 def input_model(infile):
     """ Loads in a pre-trained model file and converts it into a dictionary based model.
@@ -297,16 +296,11 @@ def input_model(infile):
     return model
 
 ## Generate from trigram model ##
-generate_text(trigram, 3, 300, br=False)
+generate_text(trigram, 3, 300)
 
 ## Generate from model-br ##
 model_br = input_model('model-br.en')
-generate_text(model_br, 3, 300, br=True)
-print(perplexity(data_valid, model_br, 3))
-
-## Other langs ## 
-generate_text(trigramEs,3,300,br=False)
-generate_text(trigramDe,3,300,br=False)
+generate_text(model_br, 3, 300)
 
 
 ### Q5 ###
@@ -329,36 +323,60 @@ warmupModel = {'##a':0.2, '#aa':0.2, '#ba':0.15, 'aaa':0.4, 'aba':0.6,
                'aab':0.5, 'abb':0.3, 'bab':0.65, 'bbb':0.4, '###':0.0,
                '#a#':0.1, '#b#':0.1, 'aa#':0.1, 'ab#':0.1, 'ba#':0.1,'bb#':0.1}
                
-print(perplexity(['##abaab#'],warmupModel,3))  
+print(perplexity(['##abaab#'],warmupModel,3))                  
                   
                   
-### OTHER ###
+### Q6 ### 
 
+def interpolation(data_train, n, lambdas):
+    """ Creates an n-gram language model using interpolation smoothing
+    Args:
+        data_train (text file): Text file to use as training data
+        n (int): desired size of n_grams
+        lambdas (list): list of weights of length n to be weight the probability of each
+                        n-gram model. List order should be (1, 2, ..., n-1, n) for
+                        n-gram size = n
+    Returns:
+        model (dict): all n-character sequences with estimated probabilities
+    """
+    ngram_array = [dict() for i in range(n)]
+    for i in range(n):
+        ngram_array[i] = ngram_model(data_train, i + 1)
+    model = {}
+    for key, value in ngram_array[-1].items():
+        key_prob = 0
+        for i in range(n):
+            sub_key = key[-(i+1):]
+            key_prob += lambdas[i]*ngram_array[i][sub_key]
+        model[key] = key_prob
+    return model   
 
+def optimInterpolation():
+    """ Find the optimal lambda parameters for interpolation trigram smoothing model
+    Returns:
+        optim.x (ndarray): array of optimized lambda parameters
+    """
+    model = 'interpolation'
+    n = 3
+    train = data_train
+    valid = data_valid
+    optim_args = (model, n, train, valid)
+    optim = optimize.minimize(perplexity_for_optim,
+                              [0.05, 0.2, 0.75],
+                              args=optim_args,
+                              constraints=({'type': 'eq', 'fun': lambda x:  x[0] + x[1] + x[2] - 1}))
+    return optim.x
 
+## Implement interpolation ##
+lambdasInter = optimInterpolation()
+modelInter = interpolation(data_train, 3, lambdasInter)    
+              
+# Perplexities from interpolation method #
+print(perplexity(data_train, modelInter, 3))
+print(perplexity(data_valid, modelInter, 3))
+print(perplexity(data_test, modelInter, 3))
+print(perplexity(testData, modelInter, 3))
+              
+# Generate text using interpolation method #
+generate_text(modelInter, 3, 300)
 
-
-# interpolation test
-# lambdas = [0.5, 0.25, 0.25]
-# n = 3
-# ngram_array = [dict() for i in range(n)]
-# for i in range(n):
-#     ngram_array[i] = ngram_model(data_train, i + 1)
-#
-# model = {}
-# p_log = 0
-# n_gram_count = 0
-# for line in data_valid:
-#     line = preprocess_line(line)
-#     for j in range(len(line) - (n)):
-#         n_gram = line[j:j + n]
-#         n_gram_count += 1
-#         probability = 0
-#         for i in range(n):
-#             if n_gram[:i + 1] in ngram_array[i]:
-#                 probability += lambdas[i] * ngram_array[i][n_gram[:i + 1]]
-#         model[n_gram] = probability
-#         p_log += np.log2(probability)
-
-# write_to_file(model, 'interpolation.txt')
-# print(2**(-1/n_gram_count * p_log))
